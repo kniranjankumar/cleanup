d922578d6c9be7a7ebd38092c2b21abad6a95386
Niranjan <kniranjankumar.eee@gmail.com>
2019-10-16 17:32:16 -0400

    "adding training code"

--- train_clean_up.py
+++ train_clean_up.py
@@ -1,14 +1,16 @@
 import gym
 import cleanup_world
 from baselines import deepq
+import tensorflow as tf
 # from baselines.baselines.common.models import mlp
 env = gym.make('2DCleanup-v0')
-act = deepq.learn(
-        env,
-        network='cnn',
-        lr=1e-3,
-        total_timesteps=100000,
-        buffer_size=50000,
-        exploration_fraction=0.1,
-        exploration_final_eps=0.02,
-        print_freq=10)
+with tf.device('/GPU:0'):
+        act = deepq.learn(
+                env,
+                network='cnn',
+                lr=1e-3,
+                total_timesteps=100000,
+                buffer_size=50000,
+                exploration_fraction=0.1,
+                exploration_final_eps=0.02,
+                print_freq=10)
