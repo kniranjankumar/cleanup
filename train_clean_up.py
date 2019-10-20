9d6bf68bb95f6e32a3fbd7ecc251c5f53f8a255b
Niranjan <kniranjankumar.eee@gmail.com>
2019-10-20 14:45:12 -0400

    "adding training code"

--- train_clean_up.py
+++ train_clean_up.py
@@ -5,8 +5,8 @@ import cleanup_world
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines.deepq.policies import MlpPolicy
 from stable_baselines import DQN
-
-env = gym.make('CartPole-v1')
+env = gym.make('2DCleanup-v0')
+# env = gym.make('CartPole-v1')
 # env = DummyVecEnv()
 model = DQN(MlpPolicy, env, verbose=1)
 model.learn(total_timesteps=25000)
@@ -21,7 +21,7 @@ while True:
     action, _states = model.predict(obs)
     obs, rewards, dones, info = env.step(action)
     if dones:
-        env.reset()
+        break
     # env.render()
 # from baselines.baselines.common.models import mlp
 # env = gym.make('2DCleanup-v0')
