6b382f091f7aabbdcef516407749516b65f908f7
niranjan <kniranjankumar.eee@gmail.com>
2019-11-07 12:01:10 -0500

    added data file for 5 participants

--- data/combine_data.py
+++ data/combine_data.py
@@ -1,8 +1,8 @@
 import pickle as pkl
 data = []
-for i in range(4):
+for i in range(5):
     for j in range(10):
-        with open('./collected_data/subject_'+str(i+1)+'/eps_'+str(j+1)+'.pkl','rb') as f:
+        with open('./collected_data/subject_'+str(i+1)+'/eps_'+str(j)+'.pkl','rb') as f:
             data.extend(pkl.load(f))
 with open('./combined.pkl','wb') as f:
     pkl.dump(data,f)
