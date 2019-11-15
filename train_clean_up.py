0415ee6b9d44d91361c6df3562ec0d119e3f36b9
niranjan <kniranjankumar.eee@gmail.com>
2019-11-15 16:23:07 -0500

    'Fixing logging issue'

--- train_clean_up.py
+++ train_clean_up.py
@@ -18,10 +18,10 @@ env = gym.make('2DCleanup-v0')
 
 goal_selection_strategy = 'future'
 model = HER('MlpPolicy', env, DQN, n_sampled_goal=4, goal_selection_strategy=goal_selection_strategy,
-                                                verbose=1, exploration_fraction=0.5)
+                                                verbose=1, exploration_fraction=0.5, tensorboard_log='/srv/share/nkannabiran3/DQN_HER')
 print('learning')
-model.learn(total_timesteps=1000000, tb_log_name='/srv/share/nkannabiran3/DQN_HER/')
-model.save("deepq_her__cartpole")
+model.learn(total_timesteps=1000000, tb_log_name='/srv/share/nkannabiran3/DQN_HER')
+model.save("deepq_her_cartpole")
 
 # del model # remove to demonstrate saving and loading
 
