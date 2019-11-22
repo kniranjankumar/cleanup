ac9732b7fd72fa18a6b166646ef2bf8cfa5ed736
niranjan <kniranjankumar.eee@gmail.com>
2019-11-21 20:21:27 -0500

    "fixing bugs"

--- train_pick_up.py
+++ train_pick_up.py
@@ -3,45 +3,62 @@ import cleanup_world
 import os
 from stable_baselines import HER, DQN
 from options import Parser
+import tensorflow as tf
+import tensorflow.contrib.layers as tf_layers
+import numpy as np
 # from stable_baselines.her import GoalSelectionStrategy, HERGoalEnvWrapper
 # from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines.deepq.policies import CnnPolicy
-# from stable_baselines import DQN
-# parser = Parser()
-# args = parser.parse()
-# print(vars(args))
+from stable_baselines.common.policies import FeedForwardPolicy, register_policy, nature_cnn
+from stable_baselines.a2c.utils import conv, linear, conv_to_fc, batch_to_seq, seq_to_batch, lstm
+from stable_baselines.deepq.policies import FeedForwardPolicy
+
+def custom_cnn(scaled_images, **kwargs):
+    """
+    CNN from Nature paper.
+
+    :param scaled_images: (TensorFlow Tensor) Image input placeholder
+    :param kwargs: (dict) Extra keywords parameters for the convolutional layers of the CNN
+    :return: (TensorFlow Tensor) The CNN output layer
+    """
+    activ = tf.nn.relu
+    layer_1 = activ(conv(scaled_images, 'c1', n_filters=32, filter_size=4, stride=2, init_scale=np.sqrt(2), **kwargs))
+    layer_2 = activ(conv(layer_1, 'c2', n_filters=64, filter_size=2, stride=1, init_scale=np.sqrt(2), **kwargs))
+    # layer_3 = activ(conv(layer_2, 'c3', n_filters=64, filter_size=3, stride=1, init_scale=np.sqrt(2), **kwargs))
+    layer_3 = conv_to_fc(layer_2)
+    return activ(linear(layer_3, 'fc1', n_hidden=512, init_scale=np.sqrt(2)))
+    
+class PickupCnnPolicy(FeedForwardPolicy):
+    """
+    Policy object that implements DQN policy, using a CNN (the nature CNN)
+
+    :param sess: (TensorFlow session) The current TensorFlow session
+    :param ob_space: (Gym Space) The observation space of the environment
+    :param ac_space: (Gym Space) The action space of the environment
+    :param n_env: (int) The number of environments to run
+    :param n_steps: (int) The number of steps to run for each environment
+    :param n_batch: (int) The number of batch to run (n_envs * n_steps)
+    :param reuse: (bool) If the policy is reusable or not
+    :param obs_phs: (TensorFlow Tensor, TensorFlow Tensor) a tuple containing an override for observation placeholder
+        and the processed observation placeholder respectivly
+    :param dueling: (bool) if true double the output MLP to compute a baseline for action scores
+    :param _kwargs: (dict) Extra keyword arguments for the nature CNN feature extraction
+    """
+
+    def __init__(self, sess, ob_space, ac_space, n_env, n_steps, n_batch,
+                 reuse=False, obs_phs=None, dueling=True, **_kwargs):
+        super(PickupCnnPolicy, self).__init__(sess, ob_space, ac_space, n_env, n_steps, n_batch, reuse,
+                                        feature_extraction="cnn", obs_phs=obs_phs, dueling=dueling, cnn_extractor=custom_cnn,
+                                        layer_norm=False, **_kwargs)
+
+
 env = gym.make('2DPickup-v0')
-model = DQN(CnnPolicy, env, verbose=1,tensorboard_log='/srv/share/nkannabiran3/DQN/',
+model = DQN(PickupCnnPolicy, env, verbose=1,tensorboard_log='/srv/share/nkannabiran3/DQN/',
             double_q=True,
             prioritized_replay=True)#,
             # prioritized_replay_alpha=0.8,
             # prioritized_replay_beta0=0.2)
 model.learn(total_timesteps=100000)
-# if args.enable_HER:
-#     model = HER('MlpPolicy', 
-#                 env, 
-#                 DQN, 
-#                 n_sampled_goal=args.num_sampled_goals, 
-#                 goal_selection_strategy=args.goal_selection_strategy,
-#                 verbose=1, 
-#                 exploration_fraction=args.exploration_fraction, 
-#                 tensorboard_log=args.tensorboard_log_path+'/'+args.name)
-# else:
-#     model = DQN(MlpPolicy, env, verbose=1,tensorboard_log='/srv/share/nkannabiran3/DQN/',
-#             double_q=True,
-#             prioritized_replay=True,
-#             prioritized_replay_alpha=0.8,
-#             prioritized_replay_beta0=0.2)
-# print('learning')
-# os.mkdir(args.tensorboard_log_path+'/'+args.name)
-# parser.save_args()
-# model.learn(total_timesteps=args.num_training_steps, tb_log_name=args.tensorboard_log_path+'/'+args.name)
-# model.save(args.name)
 
-
-# del model # remove to demonstrate saving and loading
-
-# model = DQN.load("deepq_cartpole")
 
 obs = env.reset()
 while True:
