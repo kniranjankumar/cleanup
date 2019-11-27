181a9d67fc4d0406d4923856d416fc90f893492b
Niranjan <kniranjankumar.eee@gmail.com>
2019-11-27 12:27:13 -0500

    "adding goalenv for pickup"

--- 2D_cleanup/cleanup_world/envs/pick_up_world.py
+++ 2D_cleanup/cleanup_world/envs/pick_up_world.py
@@ -21,6 +21,8 @@ class WorldObject(object):
         self.parent = None
         self.child = None
         self.direction = direction
+        self.goal_loc = loc
+        self.goal_direction = direction
 
     def turn(self, direction):
         change = -1 if direction == 'right' else 1
@@ -29,6 +31,32 @@ class WorldObject(object):
         self.direction = [direction for direction,
                           num in DIRECTIONS.items() if num == direction_num][0]
 
+    @property
+    def neighbors(self):
+        x, y = self.loc
+        up = x-1, y
+        down = x+1, y
+        left = x, y-1
+        right = x, y+1
+        neighbours = {'up': up, 'down': down, 'left': left, 'right': right}
+        #         [print(v) for k,v in neighbours.items()]
+        neighbours = {k: v for k, v in neighbours.items(
+        ) if v[0] >= 0 and v[0] < self.map.shape[0] and v[1] >= 0 and v[1] < self.map.shape[1]}
+        return neighbours
+    
+    @property
+    def empty_neighbors(self):
+        empty_neighbours = {k: v for k, v in self.neighbours.items()
+                    if self.map[v[0], v[1]] == None}
+        return empty_neighbours
+
+    @property
+    def grid_in_front(self):
+        if self.direction in self.neighbors.keys():
+            box_in_front = self.neighbors[self.direction]
+        else:
+            box_in_front = None
+        return box_in_front
 
 class PickupWorld(gym.Env):
 
@@ -45,12 +73,16 @@ class PickupWorld(gym.Env):
         self.done = True
         self.is_vectorized = is_vectorized
         self.is_random_start = is_random_start
-        if is_vectorized:
-            self.observation_space = Box(high=np.ones(
-                [self.world_size*self.world_size*2]), low=-1*np.ones([self.world_size*self.world_size*2]), dtype='float')
+        self.is_goal_env = is_goal_env
+        vector_box = Box(high=np.ones([self.world_size*self.world_size*2]), low=-1*np.ones([self.world_size*self.world_size*2]), dtype='float')
+        grid_box = Box(high=np.ones([self.world_size, self.world_size,2]), low=-1*np.ones([self.world_size, self.world_size,2]), dtype='float')
+        if self.is_goal_env:
+             self.observation_space = Dict(
+            {'observation': vector_box if self.is_vectorized else grid_box,
+             'achieved_goal': vector_box if self.is_vectorized else grid_box,
+             'desired_goal': vector_box if self.is_vectorized else grid_box})
         else:
-            self.observation_space = Box(high=np.ones(
-                [self.world_size, self.world_size,2]), low=-1*np.ones([self.world_size, self.world_size,2]), dtype='float')
+            self.observation_space = vector_box if self.is_vectorized else grid_box
 
     def update_location(self, item_name, new_loc):
         item = self.items[item_name]
@@ -112,7 +144,7 @@ class PickupWorld(gym.Env):
         # print(self.agent_direction)
 
     def step(self, action):
-        action_space_str = ['forward', 'left', 'right', 'pick']
+        action_space_str = ['forward', 'left', 'right', 'end']
         action = action_space_str[action]
         self.t += 1
         rew = -0.0
@@ -123,27 +155,37 @@ class PickupWorld(gym.Env):
         elif action == 'right':
             self.items['agent'].turn('right')
         elif action == 'forward':
-            possible_actions = self.get_neighbors(agent_location)
-            # print('forward actions', possible_actions)
-            if self.items['agent'].direction in possible_actions.keys():
+            box_in_front = self.items['agent'].box_in_front
+            is_empty = not isinstance(self.map[box_in_front[0], box_in_front[1]], WorldObject)  #is grid empty?
+            if is_empty:
                 # print(self.agent_direction)
-                move_to = possible_actions[self.items['agent'].direction]
-                self.update_location('agent', move_to)
-        elif action == 'pick':
-            # print('here')
-            possible_actions = self.get_neighbors(
-                agent_location,see_objects=False)
-            if self.items['agent'].direction in possible_actions.keys():
-                box_in_front = possible_actions[self.items['agent'].direction]
-                if isinstance(self.map[box_in_front[0], box_in_front[1]], WorldObject):
-                    # pickedup object
-                    rew = 1
-                    self.done = True
+                self.update_location('agent', box_in_front)
+        elif action == 'end':
+            self.done = True
+
+            # possible_actions = self.get_neighbors(
+            #     agent_location,see_objects=False)
+            # if self.items['agent'].direction in possible_actions.keys():
+            #     box_in_front = possible_actions[self.items['agent'].direction]
+            #     if isinstance(self.map[box_in_front[0], box_in_front[1]], WorldObject):
+            #         # pickedup object
+            #         rew = 1
+            # # possible_actions = self.get_neighbors(
+            # #     agent_location,see_objects=False)
+            # # if self.items['agent'].direction in possible_actions.keys():
+            # #     box_in_front = possible_actions[self.items['agent'].direction]
+            # #     if isinstance(self.map[box_in_front[0], box_in_front[1]], WorldObject):
+            # #         # pickedup object
+            # #         rew = 1
+            # #         self.done = True
 
         obs = self.get_obs()
         if self.t == self.TIME_LIMIT:
             self.done = True
         return obs, rew, self.done, {}
+
+    def normalize_array(self, array):
+        return (array-0.5)*2
 
     @property
     def map_array(self):
@@ -152,12 +194,55 @@ class PickupWorld(gym.Env):
         for k,v in self.items.items():
             position[v.loc[0],v.loc[1]] = self.str2objID(k)
             orientation[v.loc[0],v.loc[1]] = DIRECTIONS[v.direction]
-        mat = np.stack([position/len(self.object_list),orientation/3],axis=2)-0.5
-        return mat*2
+        mat = np.stack([position/len(self.object_list),orientation/3],axis=2)
+        return self.normalize_array(mat)
+
+    @property
+    def goal_array(self):
+        position = np.zeros_like(self.map, dtype='float')
+        orientation = np.zeros_like(self.map, dtype='float')
+        for k,v in self.items.items():
+            position[v.goal_loc[0],v.goal_loc[1]] = self.str2objID(k)
+            orientation[v.goal_loc[0],v.goal_loc[1]] = DIRECTIONS[v.goal_direction]
+        mat = np.stack([position/len(self.object_list),orientation/3],axis=2)
+        return self.normalize_array(mat)
+
+
+    @property
+    def achieved_array(self):
+        position = np.zeros_like(self.map, dtype='float')
+        orientation = np.zeros_like(self.map, dtype='float')
+        #agent location
+        position[self.items['agent'].goal_loc[0],self.items['agent'].goal_loc[1]] = self.str2objID('agent')
+        orientation[self.items['agent'].goal_loc[0],self.items['agent'].goal_loc[1]] = DIRECTIONS[self.items['agent'].goal_direction]
+        #object location
+        if self.items['agent'].box_in_front is not None:
+            position[self.items['agent'].box_in_front[0],self.items['agent'].box_in_front[1]] = self.str2objID('object')
+            orientation[self.items['agent'].box_in_front[0],self.items['agent'].box_in_front[1]] = DIRECTIONS[self.items['object'].goal_direction]
+        mat = np.stack([position/len(self.object_list),orientation/3],axis=2)
+        return self.normalize_array(mat)
+
+    def create_goal(self):
+        self.goal_map = np.empty((self.world_size, self.world_size), dtype=object)
+        neighbors = self.get_neighbors(self, self.items['object'].loc, see_objects=True)
+        #choose a neighbor cell and place agent there
+        selected_cell_direction = self.np_random.choice(list(neighbors.keys()))
+        agent_location = neighbors[selected_cell_direction]
+        inverse_directions = {'up':'down', 'down':'up', 'left':'right','right':'left'}
+        agent_direction = inverse_directions[selected_cell_direction]
+        self.items['agent'].goal_loc = agent_location
+        self.items['agent'].goal_direction = agent_direction
 
     def get_obs(self):
-        return self.map_array.reshape(-1) if self.is_vectorized else self.map_array
+        if not self.is_goal_env:
+            return self.map_array.reshape(-1) if self.is_vectorized else self.map_array
+        else:
+            return {'observation':self.map_array,
+                    'achieved_goal':self.map_array,
+                    'desired_goal':self.goal_array}
 
+    def compute_reward(self, achieved_goal, desired_goal, info):
+        pass
 
 if __name__ == '__main__':
     env = PickupWorld(max_time_steps=1000)
