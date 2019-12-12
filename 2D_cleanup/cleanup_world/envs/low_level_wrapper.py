229cca70a6a89d6d2386d30d1c8d519735592aa7
Niranjan <kniranjankumar.eee@gmail.com>
2019-12-11 21:38:25 -0500

    train dqn from expert data

--- 2D_cleanup/cleanup_world/envs/low_level_wrapper.py
+++ 2D_cleanup/cleanup_world/envs/low_level_wrapper.py
@@ -5,19 +5,24 @@ import cv2
 import numpy as np
 from gym.spaces import Dict, Box, Discrete
 
-
+import pickle
 class PickupWorld(CleanupWorld):
     def __init__(self, max_time_steps=100, is_goal_env=True, render=False):
         super().__init__(max_time_steps=max_time_steps,
                          render=render)
         self.action_space = Discrete(4)
         vector_box = Box(
-            high=np.ones([self.world_size * self.world_size * 2]),
-            low=-1 * np.ones([self.world_size * self.world_size * 2]),
+            high=np.ones([self.world_size * self.world_size]),
+            low=-1 * np.ones([self.world_size * self.world_size]),
             dtype="float",
         )
         self.is_goal_env = is_goal_env
         if is_goal_env:
+            with open('./clean_data/all_trajectories.pkl','rb') as f:
+                data = pickle.load(f)
+                self.expert_state = data['observations']
+                self.expert_goals = data['goals']
+                self.expert_goal_orientations = data['orientation']
             self.observation_space = Dict(
                 {
                     "observation": vector_box,
@@ -30,31 +35,44 @@ class PickupWorld(CleanupWorld):
             
     def reset(self):
         obs = super().reset()
-        # TODO select a random object and try to pick it up
-        pickable_objects = [
-            k for k, v in self.world_objects.items() if v.is_movable]
-        pickable_objects.remove('agent')
-        pickable_objects = self.np_random.permutation(pickable_objects)
-        #pick random empty block
-        empty_blocks = np.argwhere(obs==0)
-        self.goal_location = empty_blocks[self.np_random.int(len(empty_blocks))]
+        sample_idx = self.np_random.randint(len(self.expert_state))
+        sample_state = self.expert_state[sample_idx][0]
+        print('chosen_goal',self.expert_goals[sample_idx])
+        sample_goal = int(self.expert_goals[sample_idx]/self.map.shape[0]), self.expert_goals[sample_idx]%self.map.shape[1]
+        sample_goal_orientation = self.expert_goal_orientations[sample_idx] 
+        self.set_state(sample_state)
+        self.goal_location = sample_goal
+        self.goal_orientation = sample_goal_orientation
         obs = self.get_observation_dict() if self.is_goal_env else self.get_observation_dict()['observation']
         return obs
+    # def reset(self):
+    #     obs = super().reset()
+    #     # TODO select a random object and try to pick it up
+    #     pickable_objects = [
+    #         k for k, v in self.world_objects.items() if v.is_movable]
+    #     pickable_objects.remove('agent')
+    #     pickable_objects = self.np_random.permutation(pickable_objects)
+    #     #pick random empty block
+    #     empty_blocks = np.argwhere(obs==0)
+    #     self.goal_location = empty_blocks[self.np_random.randint(len(empty_blocks))]
+    #     self.goal_orientation =  self.np_random.randint(3)
+    #     obs = self.get_observation_dict() if self.is_goal_env else self.get_observation_dict()['observation']
+    #     return obs
 
-    def reset2state(self,state):
-        obs = super().set_state(state)
-        # TODO select a random object and try to pick it up
-        pickable_objects = [
-            k for k, v in self.world_objects.items() if v.is_movable]
-        pickable_objects.remove('agent')
-        pickable_objects = self.np_random.permutation(pickable_objects)
-        self.goal_object = self.world_objects[pickable_objects[0]]
-        obs = self.get_observation_dict() if self.is_goal_env else self.get_observation_dict()['observation']
-        return obs
+    # def reset2state(self,state):
+    #     obs = super().set_state(state)
+    #     # TODO select a random object and try to pick it up
+    #     pickable_objects = [
+    #         k for k, v in self.world_objects.items() if v.is_movable]
+    #     pickable_objects.remove('agent')
+    #     pickable_objects = self.np_random.permutation(pickable_objects)
+    #     self.goal_object = self.world_objects[pickable_objects[0]]
+    #     obs = self.get_observation_dict() if self.is_goal_env else self.get_observation_dict()['observation']
+    #     return obs
 
-    @property
-    def goal_location(self):
-        return self.goal_object.location
+    # @property
+    # def goal_location(self):
+    #     return self.goal_location
 
     @property
     def box_in_front(self):
@@ -68,10 +86,29 @@ class PickupWorld(CleanupWorld):
 
     @property
     def achieved_goal_array(self):
-        array = np.zeros([self.world_size, self.world_size, 2])
+        array = np.zeros([self.world_size, self.world_size])
         loc = self.world_objects['agent'].location
-        array[loc[0],loc[1],0] = 1
-        array[loc[0],loc[1],1] = self.world_objects['agent'].matrix_orientation
+        array[loc[0],loc[1]] = 1 + self.world_objects['agent'].matrix_orientation
+        # array[loc[0],loc[1],1] = self.world_objects['agent'].matrix_orientation
+        return array
+    # @property
+    # def achieved_goal_array(self):
+    #     array = np.zeros([self.world_size, self.world_size, 2])
+    #     achieved_location = self.box_in_front
+    #     # print('achived location', achieved_location)
+    #     if achieved_location is not None:
+    #         array[achieved_location[0], achieved_location[1], :] = 1
+    #     else:
+    #         array[self.world_objects['agent'].location[0],self.world_objects['agent'].location[1],:] = 1
+    #     return array
+
+    @property
+    def desired_goal_array(self):
+        array = np.zeros([self.world_size, self.world_size])
+        array[self.goal_location[0], self.goal_location[1]] = 1+ self.goal_orientation
+        # array[self.goal_location[0], self.goal_location[1], 1] = self.goal_orientation
+        return array
+
     # @property
     # def achieved_goal_array(self):
     #     array = np.zeros([self.world_size, self.world_size, 2])
@@ -88,23 +125,6 @@ class PickupWorld(CleanupWorld):
     #     array = np.zeros([self.world_size, self.world_size, 2])
     #     array[self.goal_location[0], self.goal_location[1], :] = 1
     #     return array
-
-    @property
-    def achieved_goal_array(self):
-        array = np.zeros([self.world_size, self.world_size, 2])
-        achieved_location = self.box_in_front
-        # print('achived location', achieved_location)
-        if achieved_location is not None:
-            array[achieved_location[0], achieved_location[1], :] = 1
-        else:
-            array[self.world_objects['agent'].location[0],self.world_objects['agent'].location[1],:] = 1
-        return array
-
-    @property
-    def desired_goal_array(self):
-        array = np.zeros([self.world_size, self.world_size, 2])
-        array[self.goal_location[0], self.goal_location[1], :] = 1
-        return array
 
     @property
     def agent_neighbors(self):
@@ -137,19 +157,39 @@ class PickupWorld(CleanupWorld):
 
     def get_observation_dict(self):
         obs = super().get_observation()
+        obs[obs>=2] = 2
+        obs = obs[:,:,0]
         return {'observation': obs.reshape(-1), 'achieved_goal': self.achieved_goal_array.reshape(-1), 'desired_goal': self.desired_goal_array.reshape(-1)}
 
     def compute_reward(self, achieved_goal, desired_goal, info=None):
-        achieved_goal = achieved_goal.reshape(self.map.shape[0],self.map.shape[1],2)[:,:,0]
-        desired_goal = desired_goal.reshape(self.map.shape[0],self.map.shape[1],2)[:,:,0]
-        achieved_goal_location = np.argwhere(achieved_goal == 1)
-        desired_goal_location = np.argwhere(desired_goal == 1)
-        if len(achieved_goal_location) == 0 or len(desired_goal_location) == 0:
-            return 0
+        achieved_goal = achieved_goal.reshape(self.map.shape[0],self.map.shape[1])
+        desired_goal = desired_goal.reshape(self.map.shape[0],self.map.shape[1])
+        achieved_goal_location = np.argwhere(np.floor(achieved_goal) == 1)[0]
+        desired_goal_location = np.argwhere(np.floor(desired_goal) == 1)[0]
         distance = np.linalg.norm(
-            desired_goal_location[0]-achieved_goal_location[0], 1)
+            desired_goal_location-achieved_goal_location, 1)
+        orientation_diff = abs(achieved_goal[achieved_goal_location[0],achieved_goal_location[1]]- desired_goal[desired_goal_location[0],desired_goal_location[1]])
+        orientation_diff = 1 if orientation_diff == 0.75 else orientation_diff*4
         # return distance
         if distance == 0:
-            return 10
+            if orientation_diff == 0:
+                return 10
+            else:
+                return 5
         else:
             return 1-distance/(self.map.shape[0]*self.map.shape[1])
+
+    # def compute_reward(self, achieved_goal, desired_goal, info=None):
+    #     achieved_goal = achieved_goal.reshape(self.map.shape[0],self.map.shape[1],2)[:,:,0]
+    #     desired_goal = desired_goal.reshape(self.map.shape[0],self.map.shape[1],2)[:,:,0]
+    #     achieved_goal_location = np.argwhere(achieved_goal == 1)
+    #     desired_goal_location = np.argwhere(desired_goal == 1)
+    #     if len(achieved_goal_location) == 0 or len(desired_goal_location) == 0:
+    #         return 0
+    #     distance = np.linalg.norm(
+    #         desired_goal_location[0]-achieved_goal_location[0], 1)
+    #     # return distance
+    #     if distance == 0:
+    #         return 10
+    #     else:
+    #         return 1-distance/(self.map.shape[0]*self.map.shape[1])
